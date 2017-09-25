667b28de26b1e5ce3f8cfce7d898524e0c429049
Christoffer Ekblom <cekmw09@student.lnu.se>
2017-09-25 10:37:33 +0200

    use console.log insted of throw

--- js/tests/assert.js
+++ js/tests/assert.js
@@ -1,6 +1,6 @@
 function assert(test, expected, actual) {
   if(expected != actual) {
-    throw "TEST [" + test + "] FAILED\n  [" + expected + "], actual : [" + actual + "]";
+  	console.log("%c%s", "color:#f00", "TEST [" + test + "] FAILED\nexpected : [" + expected + "], actual : [" + actual + "]");
   }
   else {
   	console.log("%c%s", "color:#0f0", "TEST [" + test + "] PASSED");
