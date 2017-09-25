3f16d384ef7f661f4fd77a88418b8b5fd4f567e8
Christoffer Ekblom <cekmw09@student.lnu.se>
2017-09-25 10:57:04 +0200

    uppercase

--- js/tests/assert.js
+++ js/tests/assert.js
@@ -1,4 +1,4 @@
-function assert(test, expected, actual) {
+function Assert(test, expected, actual) {
   if(expected != actual) {
   	console.log("%c%s", "color:#f00", "TEST [" + test + "] FAILED\nexpected : [" + expected + "], actual : [" + actual + "]");
   }
