d242d36b35808c1141c82c30fbcd952ab0cd903d
Christoffer Ekblom <cekmw09@student.lnu.se>
2017-10-01 15:10:47 +0200

    Refactoring

--- js/moon-sun-clock.js
+++ js/moon-sun-clock.js
@@ -10,10 +10,10 @@ MOON_SUN_CLOCK.HOURS_PER_DAY = 24;
 MOON_SUN_CLOCK.DEGREES_PER_TURN = 360;
 MOON_SUN_CLOCK.DEGREES_PER_HOUR = MOON_SUN_CLOCK.DEGREES_PER_TURN / MOON_SUN_CLOCK.HOURS_PER_DAY;
 MOON_SUN_CLOCK.MINUTES_PER_HOUR = 60;
+MOON_SUN_CLOCK.SUN_MOON_PICTURE = document.getElementById("moon-sun");
 
 MOON_SUN_CLOCK.Rotate = function() {
-  let sunMoonPic = document.getElementById("moon-sun");
-  sunMoonPic.style.transform = "rotate(" + MOON_SUN_CLOCK.GetAngle(new Date()) + "deg)";
+  MOON_SUN_CLOCK.SUN_MOON_PICTURE.style.transform = "rotate(" + MOON_SUN_CLOCK.GetAngle(new Date()) + "deg)";
 }
 
 MOON_SUN_CLOCK.GetAngle = function(time) {
